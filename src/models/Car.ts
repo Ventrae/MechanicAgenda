ae7f50c652d3f054bacc2d2709778649ae7bb98b
Filip Dobrzyniewicz <ventrae@gmail.com>
2019-12-15 02:36:24 +0100

    Trying to implement mixin for firestore usage.

--- src/models/Car.ts
+++ src/models/Car.ts
@@ -1,12 +1,13 @@
 export default class Car {
+    id: string;
     brand: string;
     model: string;
     plates: string;
 
-    constructor(brand: string, model: string, plates: string) {
+    constructor(id: string, brand: string, model: string, plates: string) {
+        this.id = id;
         this.brand = brand;
         this.model = model;
         this.plates = plates;
     }
-
 }
--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,12 +1,25 @@
 <template>
     <div>
-        profile page
+        Test ściągania jednej transakcji:<br/>
+        [ {{ tran.name }} - {{ tran.date }} ]
     </div>
 </template>
 
 <script>
+    import {firestoreRequests} from "@/mixins/firestoreRequests";
+
     export default {
-        name: "Profile"
+        name: "Profile",
+        data(){
+            return {
+                tran: undefined
+            }
+        },
+        mixins: [ firestoreRequests ],
+        created() {
+            console.log(this.getOneTransaction('WRMtYIamcPWv6pu1AIEr'));
+            this.tran = this.getOneTransaction('WRMtYIamcPWv6pu1AIEr');
+        }
     }
 </script>
 
