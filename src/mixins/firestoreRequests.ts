03f88e776020a7ca95f03bb28d91e9d2e55aba02
Filip Dobrzyniewicz <ventrae@gmail.com>
2019-12-20 16:55:22 +0100

    Trying to implement mixin for firestore usage with single element.

--- src/mixins/firestoreRequests.ts
+++ src/mixins/firestoreRequests.ts
@@ -117,35 +117,59 @@ export const firestoreRequests = {
             firestore.collection("Transactions").doc(id)
                 .get()
                 .then((transaction)=>{
-                    if(transaction.exists){
-                        x = new Transaction(
-                            transaction.id,
-                            transaction.data().name,
-                            transaction.data().date,
-                            transaction.data().comment,
-                            [
-                                new Service('mockid',"To tylko mock serwisów", 1200),
-                                new Service('mockid',"Ogarnąć relacje w firestore", 100)
-                            ],
-                            new Client('mockid',"Mock", "Klienta", "mock@gmail.com", "123456789"),
-                            new Car('mockid',"Mock", "Car", "GS12345"),
-                            transaction.data().token,
-                            new User('mockid', 'mockname', 'mockpass', false, []),
-                            transaction.data().total
-                        );
+                    return new Promise((resolve, reject) =>{
+                        if(transaction.exists){
+                            x = new Transaction(
+                                transaction.id,
+                                transaction.data().name,
+                                transaction.data().date,
+                                transaction.data().comment,
+                                [
+                                    new Service('mockid',"To tylko mock serwisów", 1200),
+                                    new Service('mockid',"Ogarnąć relacje w firestore", 100)
+                                ],
+                                new Client('mockid',"Mock", "Klienta", "mock@gmail.com", "123456789"),
+                                new Car('mockid',"Mock", "Car", "GS12345"),
+                                transaction.data().token,
+                                new User('mockid', 'mockname', 'mockpass', false, []),
+                                transaction.data().total
+                            );
+                            resolve(x);
+                        }
+                        else {
+                            console.log('Document with this ID doesn\'t exist');
+                            x = null;
+                            reject(x);
+                        }
+                    });
+                })
+                .then(data => {
+                        console.log(data);
+                        return data;
                     }
-                    else {
-                        console.log('Document with this ID doesn\'t exist');
-                        x = null;
-                    }
-                    return x;
-                });
+                );
         },
         getOneUser(id: string){
 
         },
         getOneCar(id: string){
-
+            const docRef = firestore.collection("Cars").doc(id);
+            var vm = this; //creating the reference to the Vue VM.
+            let result = new Car('','', '', '');
+            docRef.get().then(
+                function(doc){
+                    if(doc.exists){
+                        console.log(doc.data());
+                        let x = new Car(doc.id, doc.data().brand, doc.data().model, doc.data().plates);
+                        console.log(x); // x jest ok
+                        result = x;
+                    } else {
+                        console.log(`Doc data is undefined`);
+                    }
+                    return result;
+                }).catch(function(err){
+                console.log(`Oops: ${err.message}`);
+            });
         },
         getOneClient(id: string){
 
