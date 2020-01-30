4809b1998dac69189104dc8cda862b2d47d635a9
Filip Dobrzyniewicz <ventrae@gmail.com>
2020-01-30 21:10:03 +0100

    Merged all the work of team members.

--- src/components/page-agenda/queue-view.vue
+++ src/components/page-agenda/queue-view.vue
@@ -1,17 +1,20 @@
 <template>
+    <div class="single-row border-red d-flex flex-column my-4 px-5 text-left">
+        <h3>{{ user.name }}:</h3>
+        <div class="row">
+            <transaction-thumbnail
+                    v-for="transaction in transactions"
+                    :transaction="transaction"
+            />
 
-    <div class="row">
-        <transaction-thumbnail
-            v-for="transaction in transactions"
-            :transaction="transaction"
-        />
-
+        </div>
     </div>
 </template>
 
 <script>
     import TransactionThumbnail from "./transaction-thumbnail";
     import firebase from "firebase";
+
     export default {
         name: "queue-view",
         components: {TransactionThumbnail},
@@ -19,11 +22,13 @@
             transactions: {
                 type: Array | undefined,
                 required: true
+            },
+            user: {
+                type: Object,
+                required: true
             }
         },
-        methods: {
-
-        }
+        methods: {}
     }
 </script>
 
--- src/views/Agenda.vue
+++ src/views/Agenda.vue
@@ -1,47 +1,24 @@
 <template>
     <div class="page agenda" id="agenda">
 
-        <div class="single-row border-red d-flex flex-column my-4 px-5 text-left">
-            <h3>Kolejka zleceń:</h3>
-            <queue-view
-                    :transactions="transactions"
-            />
-            <md-button class="md-fab md-fab-bottom-right btn-red" @click="showCreator=!showCreator">
-                <!--fab to open creator-->
-                <md-icon>add</md-icon>
-            </md-button>
-            <transition name="fade"><!--modal with transaction creator-->
-                <create-transaction v-if="showCreator" @close="closeCreator($event)"></create-transaction>
-            </transition>
-        </div>
+        <queue-view
+                :user="{name: 'Kolejka zleceń'}"
+                :transactions="transactions"
+        />
 
-        <div class="single-row border-red d-flex flex-column my-4 px-5 text-left">
-            <h3>Filip:</h3>
-            <queue-view
-                    :transactions="transactions"
-            />
-        </div>
+        <queue-view
+                v-for="user in users"
+                :user="{name: user.name}"
+                :transactions="filterTransactions(transactions, user)"
+        />
 
-        <div class="single-row border-red d-flex flex-column my-4 px-5 text-left">
-            <h3>Damian:</h3>
-            <queue-view
-                    :transactions="transactions"
-            />
-        </div>
-
-        <div class="single-row border-red d-flex flex-column my-4 px-5 text-left">
-            <h3>Jeremi:</h3>
-            <queue-view
-                    :transactions="transactions"
-            />
-        </div>
-
-        <div class="single-row border-red d-flex flex-column my-4 px-5 text-left">
-            <h3>Tomek:</h3>
-            <queue-view
-                    :transactions="transactions"
-            />
-        </div>
+        <md-button class="md-fab md-fab-bottom-right btn-red" @click="showCreator=!showCreator">
+            <!--fab to open creator-->
+            <md-icon>add</md-icon>
+        </md-button>
+        <transition name="fade"><!--modal with transaction creator-->
+            <create-transaction v-if="showCreator" @close="closeCreator($event)"></create-transaction>
+        </transition>
 
     </div>
 </template>
@@ -49,6 +26,7 @@
 <script>
     import QueueView from "@/components/page-agenda/queue-view";
     import {firestoreRequests} from "@/mixins/firestoreRequests";
+    import {firestore} from "@/main";
     import CreateTransaction from "@/views/CreateTransaction";
 
     export default {
@@ -57,47 +35,80 @@
         components: {CreateTransaction, QueueView},
         data() {
             return {
+                users: [],
+                transactions: [],
                 showCreator: false
             }
         },
         methods: {
             closeCreator($event) {
                 this.showCreator = $event;
-            }
-        },
-        computed: {
-            transactions() {
-                return this.$store.state.transactions;
+            },
+            filterTransactions(transactions, user){
+                let result = [];
+                for(let transaction of transactions){
+                    if(transaction.user === user.id){
+                        result.push(transaction);
+                    }
+                }
+                return result;
             }
         },
         mounted() {
+
+            firestore.collection('Users').get()
+                .then(users => {
+                    users.forEach(user => {
+                        this.users.push({
+                            id: user.id,
+                            name: user.data().name
+                        })
+                    });
+                });
+
             firestore.collection('Transactions').get()
                 .then(transactions => {
                     transactions.forEach(doc => {
 
+                        const serviceFunction = async () => {
+                            let services = [];
+                            for (let s of doc.data().services) {
+                                firestore.collection('Services').doc(s).get()
+                                    .then(service => {
+                                        services.push({
+                                            description: service.data().description,
+                                            price: service.data().price
+                                        });
+                                    });
+                            }
+                            return services;
+                        };
 
-
-
-                       this.transactions.push({
-                           name: doc.data().name,
-                           date: doc.data().date,
-                           comment: doc.data().comment,
-                           client: {
-                               name: doc.data().client.name,
-                               surname: doc.data().client.surname,
-                               email: doc.data().client.name,
-                               phone: doc.data().client.phone
-                           },
-                           car: {
-                               brand: doc.data().car.brand,
-                               model: doc.data().car.model,
-                               plates: doc.data().car.plates,
-                           },
-                           token: doc.data().token,
-                           total: doc.data().total,
-                           services: []
-                       })
+                        serviceFunction().then(serv => {
+                            this.transactions.push({
+                                name: doc.data().name,
+                                date: doc.data().date,
+                                comment: doc.data().comment,
+                                client: {
+                                    name: doc.data().client.name,
+                                    surname: doc.data().client.surname,
+                                    email: doc.data().client.name,
+                                    phone: doc.data().client.phone
+                                },
+                                car: {
+                                    brand: doc.data().car.brand,
+                                    model: doc.data().car.model,
+                                    plates: doc.data().car.plates,
+                                },
+                                token: doc.data().token,
+                                total: doc.data().total,
+                                services: serv,
+                                user: doc.data().user
+                            })
+                        });
                     });
+
+
                 })
         }
     }
